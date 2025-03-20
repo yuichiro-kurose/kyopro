4dd67fd217ca2b3ae30c66f6b036a39867a558e3
Yuichiro Kurose <mathmouse1111@gmail.com>
2025-03-20 21:01:53 +0900

    20250320

---
+++ seisen/p056/main.cpp
@@ -0,0 +1,41 @@
+// author: Yuichiro Kurose
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int inf = INT_MAX / 2;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int v, e, r;
+  cin >> v >> e >> r;
+  vector<vector<pair<int, int>>> g(v);
+  for (int i = 0; i < e; i++) {
+    int s, t, d;
+    cin >> s >> t >> d;
+    g[s].push_back(make_pair(t, d));
+  }
+  vector<int> dist(v, inf);
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
+  dist[r] = 0;
+  que.push(make_pair(0, r));
+  while (!que.empty()) {
+    int u = que.top().second;
+    que.pop();
+    for (auto [v, w] : g[u]) {
+      if (dist[v] > dist[u] + w) {
+        dist[v] = dist[u] + w;
+        que.push(make_pair(dist[v], v));
+      }
+    }
+  }
+  for (int i = 0; i < v; i++) {
+    if (dist[i] == inf) {
+      cout << "INF" << endl;
+    } else {
+      cout << dist[i] << endl;
+    }
+  }
+  return 0;
+}
